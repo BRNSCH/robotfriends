47b33b97092ebcc82a3f65134b343802b44c9abd
BRNSCH <chrischmitt@hotmail.com>
2022-12-20 23:25:45 -0300

    robots map

--- robotfriends/src/CardList.jsx
+++ robotfriends/src/CardList.jsx
@@ -4,14 +4,22 @@ import Card from "./Card";
 
 const CardList = ({ robots }) => {
 
-    const CardComponent = robots.map((user, id) => {
-        return <Card key={id} id={robots[id].id} name={robots[id].name} email={robots[id].email} />
-    })
+    
 
     return (
         
         <div className='cards'>
-            {CardComponent}
+            {
+                robots.map((user, id) => {
+                return (
+                    <Card key={id}
+                        id={robots[id].id}
+                        name={robots[id].name}
+                        email={robots[id].email}
+                    />)
+                }
+                )
+            }
            
         </div>
     )
